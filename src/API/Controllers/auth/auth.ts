6c15a51662aee1361ea8c335fc4edd7bded13618
Jupi <spacejupiter0@gmail.com>
2024-04-30 11:50:02 +0200

    some adjustments

--- src/API/Controllers/auth/auth.ts
+++ src/API/Controllers/auth/auth.ts
@@ -4,12 +4,13 @@ import { LoginBody, RegisterBody } from "../../types/auth/auth";
 export function loginHandler(req: Request, res: Response)
 {
     const loginBody = req.body as LoginBody;
+
     res.status(200).send("login");
 }
 
 export function RegisterHandler(req: Request, res: Response)
 {
     const registerBody = req.body as RegisterBody;
-    
+
     res.status(200).send("register");
 }
