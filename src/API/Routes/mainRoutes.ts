cfea5c897a55ac0d9f17c28571afbe1a56753f40
EzzOps <3202111112@alexu.edu.eg>
2024-05-13 22:35:03 +0300

    Add designer routes and controller functions

--- src/API/Routes/mainRoutes.ts
+++ src/API/Routes/mainRoutes.ts
@@ -2,9 +2,11 @@ import express from "express";
 import authRoutes from "./auth/AuthRoutes";
 import authLocalRoutes from "./auth/passportRoutes/LocalLoginRoutes";
 import userActionsRoutes from "./user/userRoutes"
+import designerRoutes from './designer/designerRoutes';
 
 export const apiRoutes = express.Router();
 
 apiRoutes.use("/api/v1" , authRoutes)
 apiRoutes.use("/api/v1/local" , authLocalRoutes)
 apiRoutes.use("/api/v1/user" , userActionsRoutes)
+apiRoutes.use("/api/v1/designer" , designerRoutes)
