f50536b23456a571deee94fa6db9412c1fe41681
Ezz Ashraf AboAlhamed <73319030+EzzOps@users.noreply.github.com>
2024-05-13 11:19:37 +0300

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,52 +1,53 @@
-// src/index.ts
-import express, { Request, Response } from 'express';
+import express from 'express';
 import 'dotenv/config';
-import {apiRoutes} from "./API/Routes/mainRoutes";
+import { apiRoutes } from "./API/Routes/mainRoutes";
 import cors from 'cors';
 import cookieParser from 'cookie-parser';
-import { Pool } from 'pg';
 import session from 'express-session';
-import expressSession from 'express-session';
+import { Pool } from 'pg';
 import { passportLocal } from './API/Controllers/auth/log_in/passportLogin/localStrategy/LoginController';
 import { ResStatus } from './API/Exceptions/main';
+const connectPgSimple = require('connect-pg-simple');
 
 const app = express();
-const PORT = 3005;
+const PORT = process.env.PORT || 3005;
+const SESSION_SECRET = process.env.SESSION_SECRET || "secret_key";
 
-//passport configuration steps
-const pgSession = require('connect-pg-simple')(expressSession)
-const poolInstance = new Pool({
-  connectionString: process.env.DATABASE_URL
-})
-const postgreStore = new pgSession({
-  pool: poolInstance,
+// Database and session configuration
+const pgSession = connectPgSimple(session);
+const pool = new Pool({ connectionString: process.env.DATABASE_URL });
+const store = new pgSession({
+  pool: pool,
   createTableIfMissing: true,
-})
-app.use(cookieParser())
-app.use(cors({credentials: true}))
-app.use(express.json())
-app.use(express.urlencoded({extended:true}))
+});
+
+// Middleware configuration
+app.use(cors({
+  credentials: true,
+  origin: ['https://5173-misteam24-khoyoutfronte-kfuj7jrx19c.ws-eu111.gitpod.io'] // Adjust based on your front-end deployment
+}));
+app.use(cookieParser());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use(session({
-  store: postgreStore,
-  secret: process.env.SESSION_SECRET || "secret_key",
+  store: store,
+  secret: SESSION_SECRET,
   resave: false,
   saveUninitialized: false,
-  cookie: { 
+  cookie: {
     maxAge: 30 * 24 * 60 * 60 * 1000, // 30 days
-    sameSite : true,
-    httpOnly : true,
+    sameSite: 'lax', // Adjust according to your requirements
+    httpOnly: true
   }
-}))
-app.use(passportLocal.session())
-app.use(passportLocal.initialize())
-//
+}));
+app.use(passportLocal.initialize());
+app.use(passportLocal.session());
 
-//routes
+// Routing
 app.use(apiRoutes);
-app.all( "*" , (req : Request , res : Response) => 
-    res.status(ResStatus.PAGE_NOT_FOUND).send("This page in not found!"))
-//
+app.all("*", (req, res) => res.status(ResStatus.PAGE_NOT_FOUND).send("This page is not found!"));
 
+// Server activation
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
