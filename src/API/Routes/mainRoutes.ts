e6588e082e51bc7630016cd8e335affa4dc0552b
Jupi <spacejupiter0@gmail.com>
2024-05-14 13:28:22 +0300

    notification deployment

--- src/API/Routes/mainRoutes.ts
+++ src/API/Routes/mainRoutes.ts
@@ -2,9 +2,11 @@ import express from "express";
 import authRoutes from "./auth/AuthRoutes";
 import authLocalRoutes from "./auth/passportRoutes/LocalLoginRoutes";
 import userActionsRoutes from "./user/userRoutes"
+import notifications from "./notification/notificationsRoutes";
 
 export const apiRoutes = express.Router();
 
-apiRoutes.use("/api/v1" , authRoutes)
-apiRoutes.use("/api/v1/local" , authLocalRoutes)
-apiRoutes.use("/api/v1/user" , userActionsRoutes)
+apiRoutes.use("/api/v1", authRoutes)
+apiRoutes.use("/api/v1/local", authLocalRoutes)
+apiRoutes.use("/api/v1/user", userActionsRoutes)
+apiRoutes.use("/api/v1/notifications", notifications)
